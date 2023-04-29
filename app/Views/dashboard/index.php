79fa516593c7c3a9662e97187b425ae3b737e005
stefanfredik <stefanfredik@gmail.com>
2023-04-29 10:56:55 +0800

    major update

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -2,10 +2,10 @@
 <?= $this->section("content"); ?>
 
 <div class="row text-center mb-5">
-    <div>
-        <h2 class="text-white">Halo Admin</h2>
-        <h4 class="text-white">Selamat datang di <?= APP_NAME; ?></h4>
-        <img width="100" class="img-fluid" src="/assets/img/logo.png" alt="">
+    <div class="m-3 p-5 border rounded bg-white text-black">
+        <img width="100" class="img-fluid m-2" src="/assets/img/logo.png" alt="">
+        <h2 class="">Halo Admin</h2>
+        <h4 class="">Selamat datang di <?= ucfirst(APP_DESC); ?></h4>
     </div>
 </div>
 
--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -4,7 +4,7 @@
 
     <div class="card card-icon mb-4">
         <div class="row g-0">
-            <div class="col-auto card-icon-aside bg-primary"><i class="me-1 text-white-50" data-feather="users"></i></div>
+            <div class="col-auto card-icon-aside bg-green"><i class="me-1 text-white-50" data-feather="users"></i></div>
             <div class="col">
                 <div class="card-body py-5">
                     <h5 class="card-title"><?= $title; ?></h5>
@@ -14,9 +14,9 @@
         </div>
     </div>
 
-    <button data-url="<?= '/' . $meta['url'] . '/tambah'; ?>" class="m-2 btn btn-outline-primary" onclick="add(this)"><i class="bi bi-plus-circle mx-1"></i>Tambah Data</button>
+    <button data-url="<?= '/' . $meta['url'] . '/tambah'; ?>" class="my-2 btn btn-green" onclick="add(this)"><i class="bi bi-plus-circle mx-1"></i>Tambah Data</button>
     <div class="card mb-4">
-        <div class="card-header">Table User</div>
+        <div class="card-header bg-green text-white">Table User</div>
         <div id="data" class="card-body">
 
         </div>
